463c2715c42d9c2fa088e3e23ac06590a1748dc7
Karishma Srikanth <krs8rb@virginia.edu>
2023-09-07 13:50:36 -0400

    update for recent changes

--- Scripts/20230817_OPU1_COP22_check.R
+++ Scripts/20230817_OPU1_COP22_check.R
@@ -3,7 +3,7 @@
 # REF ID:   c461cfab 
 # LICENSE:  MIT
 # DATE:     2023-08-18
-# UPDATED: 
+# UPDATED:  2203-09-06
 
 # DEPENDENCIES ------------------------------------------------------------
 
@@ -25,7 +25,7 @@ library(tameDP)
 load_secrets()
 
 df_msd <- si_path() %>% 
-  return_latest("MER_Structured_Datasets_PSNU_IM_FY21-23_20230616_v2_1_South Africa") %>% 
+  return_latest("MER_Structured_Datasets_PSNU_IM_FY21-24_20230811_v1_1_South Africa") %>% 
   read_psd()
 
 # 1. USAID DREAMS Shifts: PP PREV targets for 15-19 shift from one IP to another
@@ -34,8 +34,11 @@ df_msd %>%
   filter(funding_agency == "USAID",
          indicator %in% c("PP_PREV"),
          fiscal_year == 2023,
-         ageasentered == "15-19",
-         mech_code %in% c("160611", "80008")) %>% 
+         standardizeddisaggregate == "Age/Sex",
+         # ageasentered == "15-19"
+         # ,
+          mech_code %in% c("160611", "80008")
+         ) %>% 
   group_by(operatingunit,funding_agency, mech_code, prime_partner_name, ageasentered) %>% 
   summarise(across(c(targets), sum, na.rm = TRUE)) 
 
@@ -56,10 +59,11 @@ ovc_check <- df_msd %>%
   filter(funding_agency == "USAID",
          indicator %in% c("OVC_SERV"),
          fiscal_year == 2023,
-         ageasentered == "10-14",
+         snu1 == "nw North West Province",
+         #ageasentered == "10-14",
          standardizeddisaggregate == "Age/Sex/Preventive",
          mech_code %in% c("14631", '81904', "86129")) %>% 
-  group_by(operatingunit,funding_agency, mech_code, ageasentered) %>% 
+  group_by(operatingunit,funding_agency, mech_code, trendscoarse) %>% 
   summarise(across(c(targets), sum, na.rm = TRUE)) %>% 
   rename(msd_targets = targets)
 
@@ -93,11 +97,40 @@ opu_df %>%
 
 df_msd %>% 
   filter(
-    funding_agency == "USAID",
+    #funding_agency == "USAID",
     indicator %in% c("OVC_HIVSTAT"),
-    #mech_code == "11500",
+    mech_code == "11500",
     fiscal_year == 2023) %>% 
   #count(standardizeddisaggregate, otherdisaggregate)
   group_by(operatingunit,funding_agency, mech_code, prime_partner_name) %>% 
   summarise(across(c(targets), sum, na.rm = TRUE))
 
+#general checks
+
+df_msd %>% 
+  clean_indicator() %>% 
+  filter(fiscal_year == 2023,
+         standardizeddisaggregate == "Age/Sex",
+         indicator == "PP_PREV") %>% 
+  group_by(fiscal_year, indicator,
+          # @, snuprioritization,
+           trendscoarse
+  ) %>% 
+  summarise(across(c(targets), sum, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  pivot_wider(names_from = "snuprioritization", values_from = targets) %>% 
+  View()
+
+
+df_msd %>% 
+  clean_indicator() %>% 
+  filter(fiscal_year == 2023,
+         standardizeddisaggregate %in% c("Age/Sex/ProgramStatusCaregiver", "Age/Sex/ProgramStatus", "Age/Sex/DREAMS", "Age/Sex/Preventive")) %>% 
+  group_by(fiscal_year, indicator,
+           snuprioritization,
+           trendscoarse
+  ) %>% 
+  summarise(across(c(targets), sum, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  pivot_wider(names_from = "snuprioritization", values_from = targets) %>% 
+  View()
